0dcb2f377e4731ed2e721c3cec964b667694e5d0
Tom Crayford <tcrayford@googlemail.com>
2014-07-27 15:30:56 +0100

    move ui into resources

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-miniprofiler "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :resource-paths ["ui"]
+  :resource-paths ["resources"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
