13196135739c985c34099720ddc1b589a5a39521
Tom Crayford <tcrayford@googlemail.com>
2014-07-31 23:39:34 +0100

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-miniprofiler "0.1.9-SNAPSHOT"
+(defproject clojure-miniprofiler "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :resource-paths ["resources"]
   :url "http://example.com/FIXME"
