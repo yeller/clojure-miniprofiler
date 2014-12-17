17b901e00b6e7b4aabfa23fd0d245a33dc7c8a84
Tom Crayford <tcrayford@googlemail.com>
2014-12-17 12:11:41 +0000

    start using fipp

--- bench/clojure_miniprofiler/bench.clj
+++ bench/clojure_miniprofiler/bench.clj
@@ -2,7 +2,9 @@
   (:require [criterium.core :refer [bench quick-bench]]
             [clojure-miniprofiler :refer [get-stacktrace-info wrap-miniprofiler]]
             [clojure-miniprofiler.store :as store]
-            [clojure-miniprofiler.example :as example]))
+            [clojure-miniprofiler.example :as example]
+            clojure.pprint
+            fipp.edn))
 
 (deftype NoopStore []
   store/Storage
@@ -12,6 +14,13 @@
 (def noop-store (NoopStore.))
 
 (def app (wrap-miniprofiler example/app-routes {:store noop-store}))
+
+(def sample-query
+  '[:find ?title
+    :where
+    [?m :movie/title ?title]
+    [?m :movie/year ?year]
+    [(< ?year 1984)]])
 
 (defn -main [& args]
   (println "app-no-recording")
@@ -24,4 +33,13 @@
   (let [always-authorized (wrap-miniprofiler example/app-routes
                                              {:store noop-store
                                               :authorized? (fn [_] true)})]
-    (quick-bench (always-authorized {:request-method :get :uri "/" :server-name "localhost"}))))
+    (quick-bench (always-authorized {:request-method :get :uri "/" :server-name "localhost"})))
+
+  (println "html-pprint-via-clojure-pprint")
+  (println "=========================\n\n")
+  (quick-bench (with-out-str (clojure.pprint/pprint sample-query)))
+
+
+  (println "html-pprint-via-fipp-edn")
+  (println "=========================\n\n")
+  (quick-bench (with-out-str (fipp.edn/pprint sample-query))))
