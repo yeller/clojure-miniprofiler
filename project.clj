670ffb262ed9b63fec3e507272bec98600b2aeb7
Tom Crayford <tcrayford@googlemail.com>
2014-12-17 12:05:19 +0000

    formatting

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire  "5.3.1"]]
+                 [cheshire "5.3.1"]]
   :profiles {:dev {:dependencies [[compojure "1.1.8"]]
                    :plugins [[lein-ring "0.8.11"]]
                    :ring {:handler clojure-miniprofiler.example/app}
