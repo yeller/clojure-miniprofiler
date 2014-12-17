b568fde2aef5cfb714cd82b79e8d5bbd5bff35a1
Tom Crayford <tcrayford@googlemail.com>
2014-12-17 21:36:47 +0000

    change current time again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-miniprofiler "0.2.6"
+(defproject clojure-miniprofiler "0.2.7"
   :description "a simple but effective profiler for clojure web applications"
   :resource-paths ["resources"]
   :url "http://example.com/FIXME"
--- src/clojure_miniprofiler.clj
+++ src/clojure_miniprofiler.clj
@@ -37,7 +37,7 @@
 (def ^:dynamic *current-miniprofiler* nil)
 
 (defn current-ms []
-  (long (/ (float (System/nanoTime)) 1000000)))
+  (System/currentTimeMillis))
 
 (defn ms-since-start []
   (distance-of-ns-time (get @*current-miniprofiler* :start-ns) (System/nanoTime)))
